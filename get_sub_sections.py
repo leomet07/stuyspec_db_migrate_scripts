8da5849f3f608326aa6a536e3dd7487ebd67b5ea
leomet07 <leomet07@gmail.com>
2023-02-21 11:20:19 -0500

    create script to get sub_sections

---
+++ get_sub_sections.py
@@ -0,0 +1,32 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import os 
+import pymongo
+
+CONNECTION_STR = os.getenv("MONGODB_URI")
+
+client = pymongo.MongoClient(CONNECTION_STR)
+
+db = client.devdb
+articles_collection = db.articles
+
+
+articles = list(articles_collection.find({}))
+
+print("Num of articles: ", len(articles))
+
+
+sub_sections_set = set()
+
+
+for article in articles:
+
+	article = dict(article)
+	sub_section = article.get("sub_section")
+	if sub_section != None:
+		sub_sections_set.add(sub_section)
+
+		
+print(sub_sections_set)
+print("Num of sub_sections: " , len(sub_sections_set))
